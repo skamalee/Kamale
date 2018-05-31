f6253f7610f6eeed8a18f65e00b54332e4ad6c54
skamale <skamale@LNAR-PF0VU5WJ.corp.capgemini.com>
2018-05-31 09:50:21 -0700

    Commit test 1
    Change-Id: I019a578ad64458385b12e2a1c290ba1be58e5f6b
    Signed-off-by: skamale <skamale@LNAR-PF0VU5WJ.corp.capgemini.com>

---
+++ TestingGit/src/Try1.java
@@ -0,0 +1,9 @@
+
+public class Try1 {
+
+	public static void main(String[] args) {
+		System.out.println("Try1");
+		System.out.println("Try11");
+	}
+
+}
---
+++ TestingGit/src/Try2.java
@@ -0,0 +1,10 @@
+
+public class Try2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Try2");
+		System.out.println("Try22");
+	}
+
+}
